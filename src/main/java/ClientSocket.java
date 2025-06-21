7949cac4453a921d83f0d069c68fdba5b844ec41
Hacker01russ <dehup182022@yandex.com>
2025-06-21 12:22:00 +0300

    commitModification

--- src/main/java/ClientSocket.java
+++ src/main/java/ClientSocket.java
@@ -9,15 +9,29 @@ public class ClientSocket {
         System.out.println("Client");
         String host = "localhost";
         int port = 25565;
+
+        BufferedReader in = null;
+        PrintWriter out = null;
         try(Socket clientSocket = new Socket(host,port)){
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out = new PrintWriter(clientSocket.getOutputStream(),true);
             out.println("GET/HTTP/1.1 \n"+"Host: netology.ru");
             String resp = in.readLine();
             System.out.println(resp);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+        finally {
+            try {
+                if(in != null && out !=null){
+                    in.close();
+                    out.close();
+                }
+            }
+            catch (IOException ex){
+                System.out.println(ex.getMessage());
+            }
         }
     }
 }
--- src/main/java/ServerSocket.java
+++ src/main/java/ServerSocket.java
@@ -2,12 +2,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.*;
+import java.net.Socket;
 
-public class Main {
+public class ServerSocket {
     public static void main(String[]args) throws IOException {
         System.out.println("Server");
-        ServerSocket serverSocket = new ServerSocket(25565);
+        java.net.ServerSocket serverSocket = new java.net.ServerSocket(25565);
         Socket clientSocket = serverSocket.accept();
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
